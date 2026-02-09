08b6c836f3529f8e85e122a7d3780b04c5dcdf72
NomanRafique <naumanrafique10@gmail.com>
2026-02-09 15:38:38 +0500

    LLM OCr working

--- llm_ocr.py
+++ llm_ocr.py
@@ -6,8 +6,10 @@ import base64
 
 import fitz  # PyMuPDF
 from PIL import Image
+from dotenv import load_dotenv
 from openai import OpenAI
 
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
